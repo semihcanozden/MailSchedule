65522ea5a4f3ad27e184f4069b28eb5ba6cf7037
semihcanozden <can.oz.den.semih@gmail.com>
2022-02-14 14:46:08 +0300

    first_commit

---
+++ MailSchedule/MailSchedule/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MailSchedule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
